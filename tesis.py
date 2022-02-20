3190b0f54cc4627a8d228a1420ead226c3d9f3b6
Mario Lopez <00046317@uca.edu.sv>
2022-02-19 22:32:48 -0600

    Setup + training data esp

--- tesis.py
+++ tesis.py
@@ -10,19 +10,14 @@ import sys
 from pdf2image import convert_from_path
 from nltk.tokenize import word_tokenize
 
+import matplotlib
+import matplotlib.pyplot as plt
+import skimage.io
+from skimage.color import rgb2gray
+
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('conll2002')
-
-#with open("Bethesda-00046517.pdf", "rb") as f:
-#    pdf = pdftotext.PDF(f)
-#
-#contador = 1
-#for pagina in pdf:
-#    print("pagina" + " "+str(contador))
-#    print(pagina)
-#    contador+=1
-
 
 # Path of the pdf
 PDF_file = "articulo.pdf"
@@ -44,6 +39,15 @@ f = open(outfile, "a", encoding="utf-8")
   
 for i in range(1, filelimit + 1):
     filename = "pagina_"+str(i)+".jpg"
+    binary_global = image > threshold_otsu(image)
+    blurred = skimage.filters.gaussian(image)
+
+    window_size = 25
+    thresh_niblack = threshold_niblack(image, window_size=window_size, k=0.8)
+    thresh_sauvola = threshold_sauvola(image, window_size=window_size)
+    thresh_otsu = threshold_otsu(image)#, window_size=window_size)
+    thresh_otsu_blur = threshold_otsu(blurred)
+    # ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     text = str(((pytesseract.image_to_string(Image.open(filename),lang='spa'))))
     text = text.replace('-\n', '')    
     f.write(text)
