7905e355c4690095a2a6268c422ac6e837340246
Ricardo Gómez <00108517@uca.edu.sv>
2022-02-20 00:32:54 -0600

    Agregando tagger español

--- tesis.py
+++ tesis.py
@@ -6,10 +6,12 @@ import nltk
 from PIL import Image
 import pytesseract
 import sys
-
+from nltk.tag import StanfordPOSTagger
 from pdf2image import convert_from_path
 from nltk.tokenize import word_tokenize
-
+import os
+java_path = "C:/Program Files/Java/jdk1.8.0_301/bin/java.exe"
+os.environ['JAVAHOME'] = java_path
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('conll2002')
@@ -22,6 +24,10 @@ nltk.download('conll2002')
 #    print("pagina" + " "+str(contador))
 #    print(pagina)
 #    contador+=1
+
+tagger="C:\\Users\\ragq1\\Desktop\\Tesis\\Tesis\\stanford-postagger-full-2020-11-17\\models\\spanish-ud.tagger"
+jar="C:\\Users\\ragq1\\Desktop\\Tesis\\Tesis\\stanford-postagger-full-2020-11-17\\stanford-postagger.jar"
+
 
 
 # Path of the pdf
@@ -44,7 +50,7 @@ f = open(outfile, "a", encoding="utf-8")
   
 for i in range(1, filelimit + 1):
     filename = "pagina_"+str(i)+".jpg"
-    text = str(((pytesseract.image_to_string(Image.open(filename),lang='spa'))))
+    text = str(((pytesseract.image_to_string(Image.open(filename)))))
     text = text.replace('-\n', '')    
     f.write(text)
 
@@ -58,10 +64,17 @@ training_set = [[(w.lower(),t) for w,t in s] for s in nltk.corpus.conll2002.tagg
 unigram_tagger = nltk.UnigramTagger(training_set)
 bigram_tagger = nltk.BigramTagger(training_set, backoff=unigram_tagger)
 
-sentence = """Mañana voy ir a comer pizza bien rápido. Pero luego ire a comer """
-espaniol= word_tokenize(sentence,"spanish")
-print(espaniol)
-tokens = nltk.word_tokenize(sentence, 'spanish')
-print(tokens)
+sentence = """Mario es mañoso, pero kike es delgado"""
+#espaniol= word_tokenize(sentence,language='spanish')
+#print(espaniol)
+
+tokens = nltk.word_tokenize(sentence, language='spanish')
+#tagged = nltk.pos_tag(tokens)
+#print(tagged)
+
+etiquetador=StanfordPOSTagger(tagger,jar)
+etiquetas=etiquetador.tag(tokens)
+for etiqueta in etiquetas:
+    print(etiqueta)
 
 print(bigram_tagger)
