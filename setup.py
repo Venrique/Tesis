9308a4b8a352576492d3d526449a477751eb2d60
Victor <00046517@uca.edu.sv>
2022-02-23 20:32:52 -0600

    Se hizo merge del tokenizador y ocr

--- setup.py
+++
@@ -1,4 +0,0 @@
-import shutil
-user = "mario"
-shutil.copyfile(r"./spa.traineddata",r"C:\Users\\"+user+"\\anaconda3\envs\\tesis\Library\\bin\\tessdata\\spa.traineddata")
-
--- tesis.py
+++ tesis.py
@@ -11,7 +11,7 @@ from nltk.tag import StanfordPOSTagger
 from pdf2image import convert_from_path
 from nltk.tokenize import word_tokenize
 import os
-java_path = "C:/Program Files/Java/jdk1.8.0_301/bin/java.exe"
+java_path = "tokenizer/jdk/bin/java.exe"
 os.environ['JAVAHOME'] = java_path
 
 import matplotlib
@@ -25,22 +25,12 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('conll2002')
 
-#with open("Bethesda-00046517.pdf", "rb") as f:
-#    pdf = pdftotext.PDF(f)
-#
-#contador = 1
-#for pagina in pdf:
-#    print("pagina" + " "+str(contador))
-#    print(pagina)
-#    contador+=1
-
-tagger="C:\\Users\\ragq1\\Desktop\\Tesis\\Tesis\\stanford-postagger-full-2020-11-17\\models\\spanish-ud.tagger"
-jar="C:\\Users\\ragq1\\Desktop\\Tesis\\Tesis\\stanford-postagger-full-2020-11-17\\stanford-postagger.jar"
-
-
+tagger="tokenizer\\postagger\\models\\spanish-ud.tagger"
+jar="tokenizer\\postagger\\stanford-postagger.jar"
 
 # Path of the pdf
-PDF_file = "articulo.pdf"
+docs_route = "docs/"
+PDF_file = docs_route+"articulo.pdf"
 
 dpi = 900
 paginas = convert_from_path(PDF_file, dpi)
@@ -49,7 +39,7 @@ numpag = 1
 
 for pagina in paginas:
     filename = "pagina_"+str(numpag)+".jpg"
-    pagina.save(filename, 'JPEG')
+    pagina.save(docs_route+filename, 'JPEG')
     numpag = numpag + 1
     #break
   
@@ -61,11 +51,11 @@ f = open(outfile, "a", encoding="utf-8")
   
 for i in range(1, filelimit + 1):
     filename = "pagina_"+str(i)+".jpg"
-    original = skimage.io.imread(fname=filename)
+    original = skimage.io.imread(fname=docs_route+filename)
 
     image = rgb2gray(original)
 
-    pilimage = Image.open(filename)
+    pilimage = Image.open(docs_route+filename)
     width, height = pilimage.size
     pilimage.close()
 
@@ -80,9 +70,9 @@ for i in range(1, filelimit + 1):
     plt.figure(figsize=((int)(width/dpi)*mult, (int)(height/dpi)*mult))
     plt.imshow(binary_otsu, cmap=plt.cm.gray)
     plt.axis('off')
-    plt.savefig('plt-'+filename, bbox_inches='tight')
+    plt.savefig(docs_route+'plt-'+filename, bbox_inches='tight')
 
-    text = str(((pytesseract.image_to_string(Image.open('plt-'+filename),lang='spa'))))
+    text = str(((pytesseract.image_to_string(Image.open(docs_route+'plt-'+filename),lang='spa'))))
     text = text.replace('-\n', '')
     #text = re.sub(r'[0-9]+', '', text)
     #text = re.sub(r'\n\n*', '\n', text)
@@ -90,26 +80,18 @@ for i in range(1, filelimit + 1):
 
 for i in range(1, filelimit + 1):
     filename = "pagina_"+str(i)+".jpg"
-    os.remove(filename)
-    #os.remove('plt-'+filename)
+    os.remove(docs_route+filename)
+    #os.remove(docs_route+'plt-'+filename)
   
 f.close()
 
-training_set = [[(w.lower(),t) for w,t in s] for s in nltk.corpus.conll2002.tagged_sents('esp.train')]
-unigram_tagger = nltk.UnigramTagger(training_set)
-bigram_tagger = nltk.BigramTagger(training_set, backoff=unigram_tagger)
 
-sentence = """Mario es mañoso, pero kike es delgado"""
-#espaniol= word_tokenize(sentence,language='spanish')
-#print(espaniol)
+### TOKENIZADOR ###
+sentence = "Mario es mañoso, pero kike es delgado"
 
 tokens = nltk.word_tokenize(sentence, language='spanish')
-#tagged = nltk.pos_tag(tokens)
-#print(tagged)
 
 etiquetador=StanfordPOSTagger(tagger,jar)
 etiquetas=etiquetador.tag(tokens)
 for etiqueta in etiquetas:
     print(etiqueta)
-
-print(bigram_tagger)
