42a221ff59065c3dd42696e13361bf840510a16a
Victor <00046517@uca.edu.sv>
2022-03-05 23:38:17 -0600

    Se agregó spacy

--- tesis.py
+++ tesis.py
@@ -3,6 +3,7 @@ import os
 import re
 import pdftotext
 import nltk
+import spacy
 
 from PIL import Image
 import pytesseract
@@ -10,6 +11,7 @@ import sys
 from nltk.tag import StanfordPOSTagger
 from pdf2image import convert_from_path
 from nltk.tokenize import word_tokenize
+
 import os
 java_path = "tokenizer/jdk/bin/java.exe"
 os.environ['JAVAHOME'] = java_path
@@ -21,15 +23,17 @@ from skimage.color import rgb2gray
 from skimage.filters import (threshold_otsu, threshold_niblack, threshold_sauvola)
 
 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('conll2002')
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('conll2002')
+nlp = spacy.load("es_core_news_lg")
+
 
 tagger="tokenizer\\postagger\\models\\spanish-ud.tagger"
 jar="tokenizer\\postagger\\stanford-postagger.jar"
 
 # Path of the pdf
-docs_route = "docs/"
+'''docs_route = "docs/"
 PDF_file = docs_route+"articulo.pdf"
 
 dpi = 900
@@ -74,25 +78,45 @@ for i in range(1, filelimit + 1):
 
     text = str(((pytesseract.image_to_string(Image.open(docs_route+'plt-'+filename),lang='spa'))))
     text = text.replace('-\n', '')
-    text = re.sub(r'[0-9]+', '', text)
-    text = re.sub(r'(  +)', ' ', text)
-    text = re.sub(r'( +)\n\n+', '\n\n', text)
     f.write(text)
 
 for i in range(1, filelimit + 1):
     filename = "pagina_"+str(i)+".jpg"
     os.remove(docs_route+filename)
-    #os.remove(docs_route+'plt-'+filename)
-  
+    os.remove(docs_route+'plt-'+filename)
+
+text_raw = ''
+with open(outfile) as file:
+    lines = file.readlines()
+    lines = re.sub(r'[0-9]+', '', lines)
+    lines = re.sub(r'(  +)', ' ', lines)
+    lines = re.sub(r'( +)\n\n+', '\n\n', lines)
+    text_raw = lines
+
 f.close()
 
-
+f_clean = open('clean_'+outfile, "w", encoding="utf-8")
+f_clean.write(text_raw)
+f_clean.close()
+'''
+res = [[],[],[]]
 ### TOKENIZADOR ###
-sentence = "Mario es mañoso, pero kike es delgado"
+sentence = "Apple está buscando comprar una startup del Reino Unido por mil millones de dólares."
+tokenizar = nlp(sentence)
+for word in tokenizar:
+    res[0].append(word.text+' '+word.pos_)
 
 tokens = nltk.word_tokenize(sentence, language='spanish')
 
 etiquetador=StanfordPOSTagger(tagger,jar)
 etiquetas=etiquetador.tag(tokens)
 for etiqueta in etiquetas:
-    print(etiqueta)
+    res[1].append(etiqueta)
+
+print('spacy\t\t\tstanford\t\t\tstanford2')
+i = 0
+while True:
+    print(res[0][i]+'\t\t\t'+str(res[1][i]))
+    i = i+1
+    if len(res[0])==i:
+        break
